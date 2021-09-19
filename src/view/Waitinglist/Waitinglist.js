0bcfb4ab0228b082d39eb64761dadb77f72b98a7
usamahassankhan <usamahassankhan870@gmail.com>
2021-09-19 01:04:53 -0700

    rsposnive table

--- src/view/Waitinglist/Waitinglist.js
+++ src/view/Waitinglist/Waitinglist.js
@@ -67,30 +67,50 @@ const Waitinglist = () => {
             </div>
           </div>
         </div> */}
-        <table className="table table-success table-striped  ">
-          <thead>
-            <tr>
-              <th scope="col">Name</th>
-              <th scope="col">Therapy Type</th>
-              <th scope="col">Doctor Name</th>
-              <th scope="col">Date</th>
-              <th scope="col">Status</th>
-              <th scope="col">Children</th>
-              <th scope="col">Fee</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-              <td>@mdo</td>
-              <td>@mdo</td>
-              <td>@mdo</td>
-            </tr>
-          </tbody>
-        </table>
+        <div className="table-responsive">
+          <table className="table  table-striped  table-bordered table-hover ">
+            <thead className="bg-white text-primary1">
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Therapy Type</th>
+                <th scope="col">Doctor Name</th>
+                <th scope="col">Date</th>
+                <th scope="col">Status</th>
+                <th scope="col">Children</th>
+                <th scope="col">Fee</th>
+              </tr>
+            </thead>
+            <tbody className="text-white">
+              <tr className="text-white">
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+              </tr>
+              <tr class="text-white">
+                <th scope="row">2</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+              </tr>
+              <tr class="text-white">
+                <th scope="row">3</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
