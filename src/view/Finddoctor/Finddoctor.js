ba75d9ba402c4926ce3c9f86d77cb8c855dddadd
usamahassankhan <usamahassankhan870@gmail.com>
2021-09-19 03:50:00 -0700

    searchbar

--- src/view/Finddoctor/Finddoctor.js
+++ src/view/Finddoctor/Finddoctor.js
@@ -2,11 +2,11 @@ import React from "react";
 import "./index.scss";
 const Finddoctor = () => {
   return (
-    <div className="py-3">
+    <div className=" ">
       <div class="container">
-        <div class="row height d-flex justify-content-center align-items-center">
-          <div class="col-12 col-md-8">
-            <div class="search">
+        <div class="row height d-flex justify-content-center align-items-center vh-100">
+          <div class="col-12 col-md-8 pb-5 mb-5">
+            <div class="search mb-5">
               <i class="fa fa-search"></i>{" "}
               <input
                 type="text"
