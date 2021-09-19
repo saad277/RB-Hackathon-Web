31bd6db971d00d39d8351e020782b7990410e608
absarali <44799700+absarali@users.noreply.github.com>
2021-09-19 15:12:23 +0500

    added appointmenets api

--- src/component/Fullcalender/Fullcalender.js
+++ src/component/Fullcalender/Fullcalender.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import FullCalendar from "@fullcalendar/react"; // must go before plugins
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction"; // a plugin!
+import { useSelector } from "react-redux";
+import axios from "axios";
+import { apiUrl } from "../../config";
 import "./index.scss";
 const Fullcalender = () => {
+  const id = useSelector((state) => state.profile);
+  const token = useSelector((state) => state.token);
   const handleDateClick = (arg) => {
     // bind with an arrow function
     alert(arg.dateStr);
   };
-
+  useEffect(() => {
+    axios({
+      url: `${apiUrl}appointments/${id}`,
+      method: "get",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log("error occured, status code:", err.response.status);
+      });
+  }, []);
   return (
     <FullCalendar
       plugins={[dayGridPlugin, interactionPlugin]}
